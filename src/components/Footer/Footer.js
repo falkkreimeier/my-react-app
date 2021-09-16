883c44bbebf36cf669863382356de4d991971ade
Falk Kreimeier <f.kreimeier@gmx.net>
2021-09-16 16:17:22 +0200

    Add Lukas fancy Thursday Challanges

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,80 +3,50 @@ import Gryffindor from "../img/Gryffindor.jpg";
 import Ravenclaw from "../img/Ravenclaw.jpg";
 import Slytherin from "../img/Slytherin.jpg";
 import Hufflepuff from "../img/Hufflepuff.jpg";
-import { useState } from "react";
 
-function Footer() {
-  const [button, setButtonColor] = useState("");
-
+function Footer({ activeButton, onHouseButtonClick }) {
   return (
     <div className="Footer__styling">
-      <img
-        className={`Footer__button ${
-          button === "Gryffindor" ? "footer__button--activered" : ""
-        }`}
-        onClick={() => setButtonColor("Gryffindor")}
-        class="Footer__button"
-        src={Gryffindor}
-      />
+      <button className="Footer__button">
+        <img
+          className={`Footer__img ${
+            activeButton === "Gryffindor" ? "footer__img--activered" : ""
+          }`}
+          onClick={() => onHouseButtonClick("Gryffindor")}
+          src={Gryffindor}
+        />
+      </button>
 
-      <img
-        className={`Footer__button ${
-          button === "Ravenclaw" ? "footer__button--activeblue" : ""
-        }`}
-        onClick={() => setButtonColor("Ravenclaw")}
-        class="Footer__button"
-        src={Ravenclaw}
-      />
-      <img
-        className={`Footer__button ${
-          button === "Hufflepuff" ? "footer__button--activeyellow" : ""
-        }`}
-        onClick={() => setButtonColor("Hufflepuff")}
-        class="Footer__button"
-        src={Hufflepuff}
-      />
-      <img
-        className={`Footer__button ${
-          button === "Slytherin" ? "footer__button--activegreen" : ""
-        }`}
-        onClick={() => setButtonColor("Slytherin")}
-        class="Footer__button"
-        src={Slytherin}
-      />
+      <button className="Footer__button">
+        <img
+          className={`Footer__img ${
+            activeButton === "Ravenclaw" ? "footer__img--activeblue" : ""
+          }`}
+          onClick={() => onHouseButtonClick("Ravenclaw")}
+          src={Ravenclaw}
+        />
+      </button>
+
+      <button className="Footer__button">
+        <img
+          className={`Footer__img ${
+            activeButton === "Hufflepuff" ? "footer__img--activeyellow" : ""
+          }`}
+          onClick={() => onHouseButtonClick("Hufflepuff")}
+          src={Hufflepuff}
+        />
+      </button>
+
+      <button className="Footer__button">
+        <img
+          className={`Footer__img ${
+            activeButton === "Slytherin" ? "footer__img--activegreen" : ""
+          }`}
+          onClick={() => onHouseButtonClick("Slytherin")}
+          src={Slytherin}
+        />{" "}
+      </button>
     </div>
-
-    // <div className="Footer__styling">
-    //   <img></img>
-    //   <button
-    //     // class="Footer__button"
-    //     className={button === "Gryffindor" ? "footer__button--active" : ""}
-    //     onClick={() => setButtonColor("Gryffindor")}
-    //     type="button"
-    //   >
-    //     Gryfindor
-    //   </button>
-    //   <button
-    //     className={button === "Ravenclaw" ? "footer__button--active" : ""}
-    //     onClick={() => setButtonColor("Ravenclaw")}
-    //     type="button"
-    //   >
-    //     Ravenclaw
-    //   </button>
-    //   <button
-    //     className={button === "Hufflepuff" ? "footer__button--active" : ""}
-    //     onClick={() => setButtonColor("Hufflepuff")}
-    //     type="button"
-    //   >
-    //     Hufflepuff
-    //   </button>
-    //   <button
-    //     className={button === "Slytherin" ? "footer__button--active" : ""}
-    //     onClick={() => setButtonColor("Slytherin")}
-    //     type="button"
-    //   >
-    //     Slytherin
-    //   </button>
-    // </div>
   );
 }
 
